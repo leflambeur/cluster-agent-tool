f329c5c34f778250b946469112fdc047ff251a69
wgodfrey <willg526@live.com>
2020-12-29 18:05:58 +0000

    status improvements

--- cattle/status.go
+++ cattle/status.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	_ "context"
 	"fmt"
+	_ "k8s.io/api/apps/v1"
 	_ "time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -29,7 +30,29 @@ func GetStatus(vb bool) (string, error) {
 		if err != nil {
 			panic(err.Error())
 		}
-		p += fmt.Sprintf("%d", len(pods.Items))
+		for _, pod := range pods.Items {
+		p += fmt.Sprintf("| %-40s| %-8s| %-15s|\n", pod.Name, pod.Status.Phase, pod.Spec.NodeName)
+		}
+		p += fmt.Sprintf("----\nTotal: %d", len(pods.Items))
+
+		return p, err
+	} else {
+		pods, err := clientset.CoreV1().Pods("cattle-system").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+		for _, pod := range pods.Items {
+			p += fmt.Sprintf("| %-40s| %-8s| %-15s|\n", pod.Name, pod.Status.Phase, pod.Spec.NodeName)
+		}
+		p += fmt.Sprintf("----\nTotal: %d \n", len(pods.Items))
+
+		deployments, err := clientset.AppsV1().Deployments("cattle-system").List(context.TODO(), metav1.ListOptions{})
+        if err != nil {
+        	panic(err)
+		}
+		for _, deployments := range deployments.Items {
+			p += fmt.Sprintf("| %-40s| %-2d| %-2d|", deployments.Name, deployments.Status.ReadyReplicas, deployments.Status.Replicas)
+		}
 		return p, err
 	}
 	return "Error", err
