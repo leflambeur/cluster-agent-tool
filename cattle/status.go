99e70854b7fbd5f1a586a5524fe17c21e181ad92
wgodfrey <willg526@live.com>
2020-12-30 10:57:47 +0000

    Improved Status

--- cattle/status.go
+++ cattle/status.go
@@ -3,10 +3,10 @@ package cattle
 
 import (
 	"context"
-	_ "context"
 	"fmt"
-	_ "k8s.io/api/apps/v1"
-	_ "time"
+	"strings"
+	"time"
+
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -16,6 +16,28 @@ import (
 
 func GetStatus(vb bool) (string, error) {
 	var p string
+	if vb != true {
+		var podStatus, err = GetPodStatus()
+		p += fmt.Sprintf("%s", podStatus)
+		return p, err
+	} else {
+		var podStatus, err = GetPodStatus()
+		p += fmt.Sprintf("%s", podStatus)
+		return p, err
+	}
+}
+
+func NewRow(rowcount int) string {
+	newline1 := "-"
+	res1 := strings.Repeat(newline1, rowcount)
+	return res1
+}
+
+func GetPodStatus() (string,error) {
+	var ppod string
+	var newline string
+
+	newline += fmt.Sprintf("|%-41s|%-9s|%-16s|%-16s|", NewRow(41), NewRow(9), NewRow(16), NewRow(16))
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
@@ -25,36 +47,18 @@ func GetStatus(vb bool) (string, error) {
 	if err != nil {
 		panic(err.Error())
 	}
-	if vb != true {
-		pods, err := clientset.CoreV1().Pods("cattle-system").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		for _, pod := range pods.Items {
-		p += fmt.Sprintf("| %-40s| %-8s| %-15s|\n", pod.Name, pod.Status.Phase, pod.Spec.NodeName)
-		}
-		p += fmt.Sprintf("----\nTotal: %d", len(pods.Items))
-
-		return p, err
-	} else {
-		pods, err := clientset.CoreV1().Pods("cattle-system").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err)
-		}
-		for _, pod := range pods.Items {
-			p += fmt.Sprintf("| %-40s| %-8s| %-15s|\n", pod.Name, pod.Status.Phase, pod.Spec.NodeName)
-		}
-		p += fmt.Sprintf("----\nTotal: %d \n", len(pods.Items))
-
-		deployments, err := clientset.AppsV1().Deployments("cattle-system").List(context.TODO(), metav1.ListOptions{})
-        if err != nil {
-        	panic(err)
-		}
-		for _, deployments := range deployments.Items {
-			p += fmt.Sprintf("| %-40s| %-2d| %-2d|", deployments.Name, deployments.Status.ReadyReplicas, deployments.Status.Replicas)
-		}
-		return p, err
+	pods, err := clientset.CoreV1().Pods("cattle-system").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
 	}
-	return "Error", err
-}
+	ppod += fmt.Sprintf("%s\n| %-40s| %-8s| %-15s| %-15s|\n%s", newline, "Pod Name", "Status", "Age", "Node", newline)
+	for _, pod := range pods.Items {
+		podCreationTime := pod.GetCreationTimestamp()
+		age := time.Since(podCreationTime.Time).Round(time.Second)
 
+		ppod += fmt.Sprintf("\n| %-40s| %-8s| %-15s| %-15s|\n%s", pod.Name, pod.Status.Phase,
+			age.String(), pod.Spec.NodeName, newline)
+	}
+	ppod += fmt.Sprintf("\n\nTotal: %d \n", len(pods.Items))
+	return ppod, err
+}
